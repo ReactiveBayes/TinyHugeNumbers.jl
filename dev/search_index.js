e7cffce5b3540a0d77d7b2b24c9195f65f752722
Documenter.jl <documenter@juliadocs.github.io>
2022-09-29 10:32:21 +0000

    build based on f19db55

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TinyHugeNumbers","category":"page"},{"location":"#TinyHugeNumbers","page":"Home","title":"TinyHugeNumbers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TinyHugeNumbers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TinyHugeNumbers]","category":"page"}]
+}
